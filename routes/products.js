d2f0d111532650cecb2bebcdf1d60f624f790b7d
Yandisa <yandisa@softidoc.co.za>
2024-09-09 18:43:56 +0200

    updated files

--- routes/products.js
+++ routes/products.js
@@ -30,4 +30,46 @@ router.get("/products", function (req, res, next) {
     });
 });
 
+router.get("/products/:id", function (req, res, next) {
+  const productId = req.params.id;
+  supabase
+    .from("Products")
+    .select("*")
+    .eq("id", productId)
+    .then(({ data: products, error }) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send(error);
+      } else if (products.length === 0) {
+        res
+          .status(404)
+          .json({ message: `Product with ID ${productId} not found` });
+      } else {
+        console.log(products[0]);
+        res.json(products[0]);
+      }
+    });
+});
+
+router.get("/products/:id/reviews", function (req, res, next) {
+  const productId = req.params.id;
+  supabase
+    .from("Reviews")
+    .select("*")
+    .eq("product_id", productId)
+    .then(({ data: products, error }) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send(error);
+      } else if (products.length === 0) {
+        res
+          .status(404)
+          .json({ message: `Product with ID ${productId} not found` });
+      } else {
+        console.log(products[0]);
+        res.json(products[0]);
+      }
+    });
+});
+
 module.exports = router;
